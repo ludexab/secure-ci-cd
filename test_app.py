572aaf96bc0b940f4715f64f27a95ba5575beb7e
ludexab <ileanwaabalaka@gmail.com>
2025-02-06 22:47:34 +0100

    App Unit Test

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ class TestApp(unittest.TestCase):
 		tester = app.test_client(self)
 		response = tester.get("/")
 		self.assertEqual(response.status_code, 200)
-		self.assertEqual(response.data, b"Hello, Secure DevOps!")
+    self.assertIn(b'Secure CI/CD Pipeline Demo', response.data)
 
 if __name__ == "__main__":
 	unittest.main()
