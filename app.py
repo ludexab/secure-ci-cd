c4a2404af1e5b166c4855dfc19d70482e1ae5110
ludexab <ileanwaabalaka@gmail.com>
2025-02-06 22:33:31 +0100

    Added more html content

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Hello, Secure DevOps!"
+	return '''<html>
+					<body bgcolor='gray' text='white'>
+						<h1>Secure CI/CD Pipeline Demo</h1>
+							<ul>
+								<li> Setup a basic CI/CD pipeline with GitHub Actions </li>
+								<li> Integrate security scanning into the pipeline </li>
+								<li> Automate security checks on every push </li>
+								<li> Bonus: Deploy to DockerHub </li>
+							</ul>
+					</body>'''
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000)
